eb60cfb5ff2652a7bf79ff5d9a3bae0c2d3a7b51
Businda84 <109301774+Businda84@users.noreply.github.com>
2024-08-14 19:53:13 +0300

    error

---
+++ src/components/App/App.jsx
@@ -0,0 +1,34 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Profile from "../Profile/Profile"
+import FriendList from "../FriendList"
+import TransactionHistory from "../FriendListItem"
+
+
+import userData from "../../data/userData.json"
+import friends from "../../data/friends.json"
+import transactions from "../../data/transactionHistory.json"
+
+
+function App() {
+  return (
+    <>
+          <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+    />
+    <FriendList friends={friends}/>
+
+      <TransactionHistory items={transactions} />
+    </>
+
+  
+  )
+}
+
+export default App
